097b975275e584b8b8ee0670e5eb9f2df26c6bf7
Jeffrey <33533131+160245q@users.noreply.github.com>
2018-01-26 11:38:50 +0800

    task 2

--- src/main/java/RentalCalculator.java
+++ src/main/java/RentalCalculator.java
@@ -1,18 +1,19 @@
 
-
+/*
 public class RentalCalculator {
 
 	public static double carRental(String carModel, int duration, boolean firstTimer){
 		
 		double rentalFees = 0.0;
 				
-		if (firstTimer){ //first rental
+		if (firstTimer)
+		{ //first rental
 			if (carModel.equals("Honda Fit"))
 				rentalFees = 65.0*duration*0.95;
 			else
 				rentalFees = 80.0*duration*0.95;
 		}
-			else { //subsequent rental 
+		else { //subsequent rental 
 			if (carModel.equals("Honda Fit") && (duration > 3))
 				rentalFees = 65.0*duration*0.90;
 			else if (carModel.equals("Honda Fit") && (duration <= 3))
@@ -35,6 +36,67 @@ public class RentalCalculator {
 		System.out.println("Toyota, subsequent, 5D : " + RentalCalculator.carRental("Toyota Wish", 5, false));
 		System.out.println("Honda, subsequent, 3D : " + RentalCalculator.carRental("Honda Fit", 3, false));
 		System.out.println("Toyota, subsequent, 3D : " + RentalCalculator.carRental("Toyota Wish", 3, false));
+	}*/
+	
+	
+
+public class RentalCalculator {
+
+	public static double carRental(String carModel, int duration, boolean firstTimer, boolean member){
+		
+		double rentalFees = 0.0;
+				
+		if (firstTimer){
+			if (carModel.equals("Honda"))
+				rentalFees = 65.0*duration*0.95;
+			else
+				rentalFees = 80.0*duration*0.95;
+		}  
+		else {//if customer not firstTimer 
+			if (member){ 
+			//member = true
+			//10% discount <= 3 days
+				//20% discount > 3 days
+				//handle member transactions
+				// ...
+
+				if (carModel.equals("Honda") && (duration > 3))
+					rentalFees = 65.0*duration*0.80;
+				else if (carModel.equals("Honda") && (duration <= 3))
+					rentalFees = 65.0*duration*0.90;
+				else if (carModel.equals("Toyota") && (duration > 3))
+					rentalFees = 80.0*duration*0.80;
+				else
+					rentalFees = 80.0*duration*0.90;
+				
+				
+				
+				
+				
+				
+				
+			}
+			else { //non-member
+				// no discount for non member
+				//handle non-member transactions
+				//...
+				if (carModel.equals("Honda") && (duration > 3))
+					rentalFees = 65.0*duration;
+				else if (carModel.equals("Honda") && (duration <= 3))
+					rentalFees = 65.0*duration;
+				else if (carModel.equals("Toyota") && (duration > 3))
+					rentalFees = 80.0*duration;
+				else
+					rentalFees = 80.0*duration;
+				
+			}
+		}
+		
+		return rentalFees;
 	}
+	
+
 }
+
+
 
